c21b96bff428d3852d94308343967ace983282d8
Oksana1907 <oksza79@ukr.net>
2022-01-10 23:29:38 +0200

    Added filter to code

--- Group_rating/output_of_the_form.js
+++ Group_rating/output_of_the_form.js
@@ -1,29 +1,29 @@
 let listHW = [{
-    logo: '<img src="https://lms.ithillel.ua/uploads/images/885d3b77fc0d39a12aad31af709b9bd6.jpg">',
+    logo: 'https://lms.ithillel.ua/uploads/images/885d3b77fc0d39a12aad31af709b9bd6.jpg',
     surname: "Чебикін",
     nameUser: "Антон",
     madeHW: 8,
     score: 698,
 },
-{   logo: '<img src="https://klike.net/uploads/posts/2020-08/1597994286_2.jpg">',
+{   logo: 'https://klike.net/uploads/posts/2020-08/1597994286_2.jpg',
     surname: "Mukhatasov",
     nameUser: "Borys",
     madeHW: 8,
     score: 697,
 },
-{   logo: '<img src="https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty27.jpg">',
+{   logo: 'https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty27.jpg',
     surname: "Kushrinuk",
     nameUser: "Oleksandra",
     madeHW: 7,
     score: 687,
 },
-{   logo: '<img src="https://klike.net/uploads/posts/2020-08/1597994375_6.jpg">',
+{   logo: 'https://klike.net/uploads/posts/2020-08/1597994375_6.jpg',
     surname: "Odinsov",
     nameUser: "Oleksandr",
     madeHW: 6,
     score: 580,
 },
-{   logo: '<img src="https://klike.net/uploads/posts/2020-08/1597994277_8.jpg">',
+{   logo: 'https://klike.net/uploads/posts/2020-08/1597994277_8.jpg',
     surname: "Vashenko",
     nameUser: "David",
     madeHW: 6,
@@ -38,7 +38,7 @@ document.getElementById("card_user").insertAdjacentHTML(
     "beforeend",
     `
     <tr>
-    <td>${element.logo}</td>
+    <td><img src=${element.logo} alt=""></td>
     <td>  
         <div>${element.nameUser}</div>
         <div>${element.surname}</div>
