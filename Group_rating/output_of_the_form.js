a68b164c67f53eaac2fb6a4938d277ef713ba219
Oksana1907 <oksza79@ukr.net>
2022-01-10 23:23:46 +0200

    Added filter

--- Group_rating/output_of_the_form.js
+++ Group_rating/output_of_the_form.js
@@ -2,13 +2,13 @@ let listHW = [{
     logo: '<img src="https://lms.ithillel.ua/uploads/images/885d3b77fc0d39a12aad31af709b9bd6.jpg">',
     surname: "Чебикін",
     nameUser: "Антон",
-    madeHW: 7,
+    madeHW: 8,
     score: 698,
 },
 {   logo: '<img src="https://klike.net/uploads/posts/2020-08/1597994286_2.jpg">',
     surname: "Mukhatasov",
     nameUser: "Borys",
-    madeHW: 7,
+    madeHW: 8,
     score: 697,
 },
 {   logo: '<img src="https://bigpicture.ru/wp-content/uploads/2019/04/grandbeauty27.jpg">',
@@ -20,31 +20,36 @@ let listHW = [{
 {   logo: '<img src="https://klike.net/uploads/posts/2020-08/1597994375_6.jpg">',
     surname: "Odinsov",
     nameUser: "Oleksandr",
-    madeHW: 7,
-    score: 680,
+    madeHW: 6,
+    score: 580,
 },
 {   logo: '<img src="https://klike.net/uploads/posts/2020-08/1597994277_8.jpg">',
     surname: "Vashenko",
     nameUser: "David",
-    madeHW: 7,
-    score: 675,
+    madeHW: 6,
+    score: 575,
 }];
-listHW.forEach(function(element) {
-    document.getElementById("card_user").insertAdjacentHTML(
-        "beforeend",
-        `
-        <tr>
-        <td>${element.logo}</td>
-        <td>  
-          <div>${element.nameUser}</div>
-          <div>${element.surname}</div>
-        </td>
-        <td>
-          <div>${element.madeHW}</div>
-          <div>${element.score}</div>
-        </td>
-        </tr>
-        `
+let homeworkDone = listHW.filter(function (elm) {
+        return elm.madeHW > 6;
+    });
+
+homeworkDone.forEach(function(element) {
+document.getElementById("card_user").insertAdjacentHTML(
+    "beforeend",
+    `
+    <tr>
+    <td>${element.logo}</td>
+    <td>  
+        <div>${element.nameUser}</div>
+        <div>${element.surname}</div>
+    </td>
+    <td>
+        <div>${element.madeHW}</div>
+        <div>${element.score}</div>
+    </td>
+    </tr>
+    `
         )
     }
 )
+
